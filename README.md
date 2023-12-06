ea324a8680575d1ec4b0ee0b4e16e8e696683df2
Solra Bizna <solra@bizna.name>
2023-12-05 22:19:02 -0700

    Puzzle 6 :(

--- README.md
+++ README.md
@@ -33,3 +33,13 @@ Uneventful. I'm not thrilled about needing a mut for part 2.
 Day 5 penance sketch: "Steampunk Rock Paper Scissors"
 
 This one was very very dense. I first implemented it without much optimization, but my spidey sense tingled and when I saw part two I saw that it was right to tingle. I ended up implementing a brute force solution first. I knew how to solve it properly, but I had to prove to my audience that rust is *fast*. So, with the aid of Rayon, some sorting, and the humble binary search, I got it chewing through all 2 132 355 834 possible seeds in just 11.843 seconds. (Then I implemented it properly, and that solution takes so little time to run that I can't measure it on my Ryzen.)
+
+# Puzzle 6
+
+Day 6 penance sketch: prompt pending.
+
+This one was confusing. I was really afraid of how small the puzzle was. I expected part 2 to be a doozy. I was not afraid for the right reasons. It ended up being confusingly fast and easy to solve. I got tripped up by ilog10, and... that was all.
+
+?????????
+
+I expected to have to do math...
---
+++ src/bin/advent_2023_06.rs
@@ -0,0 +1,55 @@
+#[derive(Debug)]
+struct Race {
+    /// Number of milliseconds the race will last
+    time: u64,
+    /// Number of millimeters the recordholder went
+    distance: u64,
+}
+
+impl Race {
+    fn get_num_solutions(&self) -> u64 {
+        (0..self.time)
+            .map(|charge_time| (self.time - charge_time) * charge_time)
+            .filter(|distance| *distance > self.distance)
+            .count() as u64
+    }
+}
+
+fn main() {
+    let mut lines = std::io::stdin().lines().map(|x| x.unwrap());
+    let line = lines.next().unwrap();
+    let (_, rest) = line.split_once(':').unwrap();
+    let times: Vec<u64> = rest
+        .split(' ')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let line = lines.next().unwrap();
+    let (_, rest) = line.split_once(':').unwrap();
+    let distances: Vec<u64> = rest
+        .split(' ')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let races: Vec<Race> = times
+        .into_iter()
+        .zip(distances)
+        .map(|(time, distance)| Race { time, distance })
+        .collect();
+    println!(
+        "Part 1 solution: {}",
+        races.iter().map(Race::get_num_solutions).product::<u64>(),
+    );
+    let superrace = races.iter().fold(
+        Race {
+            time: 0,
+            distance: 0,
+        },
+        |total, race| Race {
+            time: total.time * 10u64.pow(race.time.ilog10() + 1) + race.time,
+            distance: total.distance * 10u64.pow(race.distance.ilog10() + 1)
+                + race.distance,
+        },
+    );
+    println!("Part 2 solution: {}", superrace.get_num_solutions());
+}
